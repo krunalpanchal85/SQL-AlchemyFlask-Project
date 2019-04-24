a42304eec329788391aaa601da64661a47dee6fa
krunalpanchal85 <kcpanchal2085@gmail.com>
2019-04-23 20:02:59 -0400

    Homework!

---
+++ Climate-App.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>`and`/api/v1.0/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def PRCP():
+    """Return a list of all station and its precipitation"""
+    # Query all passengers
+    results = session.query(Measurement.date,Measurement.prcp).\
+        group_by(Measurement.date).order_by(func.sum(Measurement.prcp).desc()).all()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    PRCP = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        PRCP.append(prcp_dict)
+
+    return jsonify(PRCP)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of Stations"""
+    # Query all Stations
+    stations = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(stations))
+
+    return jsonify(all_stations)   
+
+@app.route("/api/v1.0/tobs")
+def TOBS():
+    """Return a list of dates and TOBS"""
+    # Query all passengers
+
+    TOBS = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date >= '2010-08-23').all()
+
+    # Convert list of tuples into normal list
+    all_TOBS = list(np.ravel(TOBS))
+
+    return jsonify(all_TOBS)
+
+@app.route("/api/v1.0/<start>")
+def DTOBS():
+    """Return a list of dates and TOBS"""
+    # Query all passengers
+    start= (Measurement.date >= '2016-08-23')
+    DTBOS = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).group_by(Measurement.date).all() 
+
+    # Convert list of tuples into normal list
+    all_DTOBS = list(np.ravel(DTBOS))
+
+    return jsonify(all_DTOBS)
+
+@app.route("/api/v1.0/<start>")
+def SETOBS():
+    """Return a list of dates and TOBS"""
+    # Query all passengers
+    start= Measurement.date >= '2016-08-23'
+    end = Measurement.date <= '2017-08-23'
+    SETBOS = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all() 
+
+    # Convert list of tuples into normal list
+    all_SETOBS = list(np.ravel(SETBOS))
+
+    return jsonify(all_SETOBS)
+
+if __name__ == '__main__':
+    app.run(debug=True)
